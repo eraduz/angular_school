f3defa9ddefc5c2baee46f81f50586db2fcd3b2b
Reda <reda@eraduz.me>
2019-09-24 13:00:02 +0200

    added routing & sidebar

--- angularles4/src/app/app-routing.module.ts
+++ angularles4/src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MeComponent } from './me/me.component';
+import { MoviesComponent } from './movies/movies.component';
+import { SchoolComponent } from './school/school.component';
+import { WorkComponent } from './work/work.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'me',
+    component: MeComponent
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent
+  },
+  {
+  path: 'school',
+  component: SchoolComponent
+  },
+  {
+    path: 'work',
+    component: WorkComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angularles4/src/app/app.module.ts
+++ angularles4/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeComponent } from './me/me.component';
+import { SchoolComponent } from './school/school.component';
+import { MoviesComponent } from './movies/movies.component';
+import { WorkComponent } from './work/work.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    SchoolComponent,
+    MoviesComponent,
+    WorkComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
